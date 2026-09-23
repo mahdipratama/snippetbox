08a99286677a9bc54dca52e73c100c82dada9766
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-09-23 09:11:23 +0700

    add file server for serving static files and update base HTML template

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// "Create a file server which serves files out of the \"./ui/static\" directory."
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// mux.Handle() function to register the file server as the handler
+	// for all URL paths that start with "/static/"
+	// for matching paths, we strip the "/static" prefix before request reaches the file server
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
