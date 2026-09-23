04954e84547ee1c38c287ef8d142ddac4a5939cc
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-09-23 11:52:55 +0700

    refactor: remove unnecessary comments and streamline logger initialization

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,22 +22,8 @@ func main() {
 
 	flag.Parse()
 
-	/*
-		log.new() to create custom logger for writting information messages
-		Parameters:
-				- destination to write the logs to (os.Stdout)
-				- string prefix message (INFO followed by a tab)
-				- flags to indicate what additional information to include joined with bitwise OR operator |
-	*/
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 
-	/*
-		log.new() to create custom logger for error messages, but with Stderr
-		Parameters:
-				- destinations to write the logs to (os.Stderr)
-				- string prefix message (ERROR followed by a tab)
-				- flags to include relevant file name and line number use log.Lshofrtfile flag
-	*/
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	app := &application{
@@ -45,26 +31,10 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
-	/*
-		Initialize a new http.Server struct.
-		set the Addr and Handler fields so that the server uses the same
-		network address and routes as before.
-		set ErrorLog field: server now uses the custom errorLog logger
-	*/
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
