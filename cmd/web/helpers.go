9d6348c995e63ab4469d3a97d19296a0955c1371
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-09-23 11:37:02 +0700

    refactor error handling in handlers and add helper functions for better code organization

---
+++ cmd/web/helpers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+// writes an error message and stack trace to the errorLog
+// then sends a generic 500 Internal Server Error response to the user
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Print(trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// sends specific satus code and corresponding description to user
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// sends 404 Not Found response to user
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
