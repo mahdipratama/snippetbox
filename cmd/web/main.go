2b4d0907ad3ea2d74093b5b62ab3666330b0c984
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-09-24 09:54:44 +0700

    feat: add SnippetModel to application struct (dependency ijection) for database interaction

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/mahdipratama/snippetbox/internal/models"
 )
 
 /*
@@ -17,6 +18,7 @@ import (
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -45,6 +47,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
