11a9571682858bef4bead026846e2e9a8ad9b075
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-09-25 08:50:26 +0700

    feat: implement snippet creation in snippetCreate handler with dummy data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -51,5 +51,14 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Display a new snipppet..."))
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
