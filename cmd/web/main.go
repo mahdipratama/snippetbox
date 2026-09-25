cadaa7d5ed602ccef082238154cef7780925d7be
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-09-25 14:17:18 +0700

    feat: implement template caching for improved snippet rendering

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/mahdipratama/snippetbox/internal/models"
@@ -16,9 +17,10 @@ import (
 */
 
 type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
-	snippets *models.SnippetModel
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
@@ -44,10 +46,17 @@ func main() {
 
 	defer db.Close()
 
+	//initialize a new template cache...
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
 	app := &application{
-		errorLog: errorLog,
-		infoLog:  infoLog,
-		snippets: &models.SnippetModel{DB: db},
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
 	srv := &http.Server{
